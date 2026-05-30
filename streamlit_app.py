c0a74400e7dd070eaa28b0e6849b79e647dfaa59
Sumna Chea <sumnachea85@gmail.com>
2026-05-30 17:41:15 -0400

    started streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+# STREAMLIT DASHBOARD
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.title("Weather Dashboard")
+st.text("Temperatures Around the World")
