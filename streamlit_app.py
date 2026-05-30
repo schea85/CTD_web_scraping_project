358681dc0085c0fe1b4ac68f66b3b912622ca388
Sumna Chea <sumnachea85@gmail.com>
2026-05-30 19:03:29 -0400

    reran program with updated data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,16 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
+import sqlite3
 import plotly.express as px
 
-st.title("Weather Dashboard")
-st.text("Temperatures Around the World")
+# load db 
+conn = sqlite3.connect("db/weather.db")
+df = pd.read_sql_query("SELECT * FROM weather", conn)
+conn.close()
+
+# streamlit
+st.title("World City Temperature Dashboard")
+st.text("Explore the Weather Around the World")
+
+st.write(df.head())
