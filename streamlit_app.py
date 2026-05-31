1c1cbed1649fb25d818212a2b075a0691e52757e
Sumna Chea <sumnachea85@gmail.com>
2026-05-30 20:43:26 -0400

    added mini city-weather dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,17 @@ conn = sqlite3.connect("db/weather.db")
 df = pd.read_sql_query("SELECT * FROM weather", conn)
 conn.close()
 
-# streamlit
+# streamlit dashboard
+
 st.title("World City Temperature Dashboard")
 st.text("Explore the Weather Around the World")
 
-st.write(df.head())
+# section 1 - display dropdown of city - weather data
+st.subheader("City Weather Data:")
+selected_city = st.selectbox("Select a City to View Weather Details", df["City"])
+filtered_city = df[df["City"] == selected_city].iloc[0]
+col1, col2, col3 = st.columns(3)
+col1.metric("City", filtered_city["City"])
+col2.write("Weather Description")
+col2.write(filtered_city["Weather Description"])
+col3.metric("Temperature", f"({filtered_city['Temperature']} °F)")
